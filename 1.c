8bcf9c465c71f120d01193da0d0d40bef838a5a6
facundoleites <leitesfacundo@gmail.com>
2017-05-28 15:53:12 -0300

    adicionar uma linha mostramdo em qual seculo você viveu mais tempo

--- 1.c
+++ 1.c
@@ -30,9 +30,20 @@ main(){
         anos_21++;
         anodaresta--;
     }
+    anos_20 = idade - anos_21;
     printf("Você já viveu : \n");
     printf("\t %.d anos ─ e uma parte do %.d ─ no século XXI \n",(anos_21-1),anoatual);
-    printf("\t %.d anos ─ e uma parte do %.d ─ no século XX \n",(idade-anos_21),(anoatual-idade));
+    printf("\t %.d anos ─ e uma parte do %.d ─ no século XX \n",anos_20,(anoatual-idade));
+    printf("\t O que quer dizer que: \n");
+    if(anos_21 > anos_20){
+        printf("\t \t Viveu a maior parte da sua vida no século XXI");
+    }else if(anos_21 == anos_20){
+        printf("\t \t Viveu ─ praticamente ─ a mesma quantidade de anos nos séculos XX e XXI");
+    }else{
+        printf("\t \t Viveu a maior parte da sua vida no século XX");
+    }
+    printf("\n");
+
     for(i=0;i <= idade;i++){
         anodaresta = anoatual - i;
         if(i == idade){
